2d0fd7d1d76f066518c853f994606fd338c26354
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2025-11-15 18:44:47 +0530

    debugging dotenv not import here

--- 08_backend/src/utils/cloudinary.utils.js
+++ 08_backend/src/utils/cloudinary.utils.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config({path: './.env'});
 import { v2 as cloudinary} from 'cloudinary';
 import fs from 'fs';
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+	api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 })
 
 const uploadOnCloudinary = async(localFilePath) => {
